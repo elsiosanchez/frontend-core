adc34e8c2098f8d5ff02bd40cf2d3d6a917ab47a
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-11-14 12:23:19 -0400

    Update fieldAmount.vue

--- src/components/ADempiere/Form/VPOS2/MainOrder/OptionLine/editLine/fieldAmount.vue
+++ src/components/ADempiere/Form/VPOS2/MainOrder/OptionLine/editLine/fieldAmount.vue
@@ -1,17 +1,19 @@
 <!--
-ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A.
-Contributor(s): Elsio Sanchez elsiosanchez15@outlook.com https://github.com/elsiosanchez
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https:www.gnu.org/licenses/>.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A.
+  Contributor(s): Elsio Sanchez elsiosanchez15@outlook.com https://github.com/elsiosanchez
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -42,18 +44,21 @@ along with this program. If not, see <https:www.gnu.org/licenses/>.
 
 <script>
 import { defineComponent, computed, ref } from '@vue/composition-api'
+
 // import lang from '@/lang'
 import store from '@/store'
+
 // // Utils and Helper Methods
 // import { formatPrice, formatQuantity } from '@/utils/ADempiere/formatValue/numberFormat'
 // import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
 // import { displayLineProductPriceValue } from '@/utils/ADempiere/dictionary/form/VPOS'
 // import { copyToClipboard } from '@/utils/ADempiere/coreUtils.js'
 import { formatPrice } from '@/utils/ADempiere/formatValue/numberFormat'
-import { isEmptyValue } from '@/utils/ADempiere'
+import { isEmptyValue } from '@/utils/ADempiere/valueUtils'
 
 export default defineComponent({
   name: 'fieldAmount',
+
   props: {
     valueAmount: {
       type: Number,
@@ -78,6 +83,7 @@ export default defineComponent({
       default: false
     }
   },
+
   setup(props) {
     // Ref
     const isFocus = ref(false)
@@ -114,6 +120,7 @@ export default defineComponent({
     function customFocusLost(event) {
       isFocus.value = false
     }
+
     return {
       // Ref
       amount,
