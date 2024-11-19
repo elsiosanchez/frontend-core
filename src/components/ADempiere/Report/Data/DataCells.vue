e3158637c128df24f3ca16ab3d3ac1898dd5e2a5
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-11-19 15:16:36 -0400

    Update DataCells.vue

--- src/components/ADempiere/Report/Data/DataCells.vue
+++ src/components/ADempiere/Report/Data/DataCells.vue
@@ -88,12 +88,10 @@ import InfoReport from '@/views/ADempiere/ReportViewerEngine/infoReport.vue'
 
 // Utils and Helper Methods
 import { isEmptyValue } from '@/utils/ADempiere/valueUtils.js'
-import { isLookup } from '@/utils/ADempiere/references'
 import { zoomIn } from '@/utils/ADempiere/coreUtils.js'
 import { isSalesTransaction } from '@/utils/ADempiere/contextUtils'
-import { DATE } from '@/utils/ADempiere/references.js'
-import { formatDate as formatDateTemp, getDateFormat } from '@/utils/ADempiere/formatValue/dateFormat'
 import { formatField } from '@/utils/ADempiere/valueFormat.js'
+
 // API Request Methods
 import { listZoomWindowsRequest } from '@/api/ADempiere/fields/zoom.js'
 
@@ -223,20 +221,12 @@ export default defineComponent({
       const rowData = row.cells[field.code]
       if (!isEmptyValue(rowData)) {
         const { display_value, value: currentValue } = rowData
-        if (!isEmptyValue(display_value) || isLookup(field.display_type)) {
-          return formatField({
-            value: display_value,
-            displayedValue: display_value,
-            displayType: field.display_type,
-            columnName: field.column_name
-          })
-        }
-        if (!isEmptyValue(currentValue)) {
-          if (!isEmptyValue(currentValue.value)) {
-            return currentValue.value
-          }
-          return currentValue
-        }
+        return formatField({
+          value: currentValue,
+          displayedValue: display_value,
+          displayType: field.display_type,
+          columnName: field.column_name
+        })
       }
     }
 
@@ -348,7 +338,6 @@ export default defineComponent({
       // Computed
       show,
       // Utilities
-      DATE,
       formatDateTemp,
       getDateFormat,
       // Métodos
