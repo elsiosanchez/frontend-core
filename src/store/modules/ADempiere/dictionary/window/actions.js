f76cbef8090ce7281a691e66e6a59a7031f85caa
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2025-01-07 17:28:52 -0400

    #239 (#364)

--- src/store/modules/ADempiere/dictionary/window/actions.js
+++ src/store/modules/ADempiere/dictionary/window/actions.js
@@ -550,6 +550,18 @@ export default {
                 id: process.id.toString(),
                 containerUuidAssociated: tabDefinition.parentUuid
               })
+                .finally(() => {
+                  const parentValues = rootGetters.getValuesView({
+                    containerUuid: tabAssociatedUuid,
+                    isOnlyColumns: true,
+                    isOnlyWithValue: true,
+                    format: 'array'
+                  })
+                  dispatch('updateValuesOfContainer', {
+                    containerUuid: process.uuid,
+                    attributes: parentValues
+                  })
+                })
             },
             // TODO: Change to string and import dynamic in component
             componentPath: () => import('@/components/ADempiere/PanelDefinition/index.vue'),
