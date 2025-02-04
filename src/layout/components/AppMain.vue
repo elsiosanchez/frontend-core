c5a2a442a12492d75e0cebfeee657f85d4b49483
Ricardo Fenomeno <78000356+Ricargame@users.noreply.github.com>
2025-02-04 09:57:32 -0400

    Fix: Change Calendar Windonw (#546)
    * Fix: Change Calendar Windonw
    
    * Fix: Change Calendar Windonw

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -89,7 +89,7 @@ export default {
   .app-main {
     /* 84 = navbar + tags-view = 50 + 34 */
     min-height: calc(100vh - 85px);
-    display: contents;
+    // display: contents;
   }
 
   .fixed-header+.app-main {
