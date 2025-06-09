a5c217cb9ce1730acdc4255ee6c709b014a8b572
Yamel Senih <yamelsenih@users.noreply.github.com>
2025-06-09 11:55:36 -0400

    Update start.sh
    References: https://github.com/solop-develop/adempiere-solop/issues/1038

--- build/start.sh
+++ build/start.sh
@@ -16,6 +16,9 @@ find -name 'app.*.js' -exec sed -i "s|http://localhost/api/|$API_URL|g" {} \;
 # Set Task Manager connection
 find -name 'app.*.js' -exec sed -i "s|http://localhost:8080/v1|$TASK_MANAGER_URL|g" {} \;
 
+# Set Deploy Version
+find -name 'app.*.js' -exec sed -i "s|vue_app_deploy_version_variable|$VUE_APP_DEPLOY_VERSION|g" {} \;
+
 
 # Start nginx web server
 nginx && tail -f /dev/null
